923f00ec603cae8838aff50edcac94343b287e80
MarionPlt <marion.playout@gmail.com>
2023-06-05 16:19:53 +0200

    update QuestionPosee

---
+++ questions/src/main/java/com/esgi/questions/controller/questionPosee/QuestionPoseeWriteController.java
@@ -0,0 +1,52 @@
+package com.esgi.questions.controller.questionPosee;
+
+import com.esgi.identity.UtilisateurGateway;
+import com.esgi.questions.domain.questionPosee.questionPoseeWriteRepository;
+import com.esgi.questions.domain.questionPosee.aggregate.QuestionPosee;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.esgi.questions.service.exceptions.ResourceNotFoundException;
+
+import java.util.Optional;
+
+public class QuestionPoseeWriteController {
+    /**
+     * Service gérant les requêtes concernant la modification des questionPosee.
+     */
+    questionPoseeWriteRepository m_questionPoseeWriteRepository;
+
+    UtilisateurGateway m_utilisateurGateway;
+
+    /**
+     * Create a question asked to the user.
+     *
+     * @param questionId    the question Id.
+     * @param utilisateurId the user Id.
+     * @return the created questionPosee Id.
+     */
+    @PostMapping
+    public long createQuestionPosee(long questionId, long utilisateurId) {
+        QuestionPosee questionPosee = new QuestionPosee(questionId, utilisateurId);
+        m_questionPoseeWriteRepository.save(questionPosee);
+        return questionPosee.getId();
+
+    }
+
+    /**
+     * Answer a questionPosee.
+     *
+     * @param questionPoseeId    the questionPosee Id.
+     * @param reponseUtilisateur the user answer.
+     */
+    @PostMapping
+    public void answerQuestionPosee(long questionPoseeId, boolean reponseUtilisateur) throws ResourceNotFoundException {
+        Optional<QuestionPosee> questionPosee = m_questionPoseeWriteRepository.findById(questionPoseeId);
+        if (questionPosee.isPresent()) {
+            questionPosee.get().answer(reponseUtilisateur);
+            m_questionPoseeWriteRepository.save(questionPosee.get());
+
+        } else {
+            throw new ResourceNotFoundException(questionPosee.getClass());
+        }
+
+    }
+}
--- questions/src/main/java/com/esgi/questions/domain/questionPosee/domainEvent/QuestionPoseeCreee.java
+++ questions/src/main/java/com/esgi/questions/domain/questionPosee/domainEvent/QuestionPoseeCreee.java
@@ -1,4 +1,10 @@
 package com.esgi.questions.domain.questionPosee.domainEvent;
 
 public class QuestionPoseeCreee {
+    /**Id de la questionPosee.*/
+    private long questionPoseeId;
+    /** Id de la question.*/
+    private long questionId;
+    /** Id du user.*/
+    private long utilisateurId;
 }
