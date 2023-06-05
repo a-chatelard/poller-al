538a5af01f24bd32e28a9cdecf99849e2ca35606
Julien Poty <julienmpoty@yahoo.fr>
2023-06-05 20:18:46 +0200

    add comments on read repositories

--- questions/src/main/java/com/esgi/questions/application/regleAttributionPoints/repositories/RegleAttributionPointsReadRepository.java
+++ questions/src/main/java/com/esgi/questions/application/regleAttributionPoints/repositories/RegleAttributionPointsReadRepository.java
@@ -7,10 +7,21 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository for read operations on a RegleAttributionPoints.
+ */
 public interface RegleAttributionPointsReadRepository extends Repository<RegleAttributionPoints, Long> {
+    /**
+     * Get a regleAttributionPoints by its Id.
+     * @return a RegleAttributionPoints.
+     */
     @Query("SELECT r FROM RegleAttributionPoints r WHERE r.id = ?1")
     Optional<RegleAttributionPoints> findById(Long regleId);
 
+    /**
+     * Get all regleAttributionPoints.
+     * @return a list of RegleAttributionPoints.
+     */
     @Query("SELECT r FROM RegleAttributionPoints r")
     List<RegleAttributionPoints> findAll();
 }
