4586bffd43a003a9d23de97af79599ae54a243f5
Antoine CHATELARD <achatelard@myges.fr>
2023-06-05 18:13:19 +0200

    Ajout gestion règles d'attribution de points

--- questions/src/main/java/com/esgi/questions/QuestionApplication.java
+++ questions/src/main/java/com/esgi/questions/QuestionApplication.java
@@ -3,13 +3,10 @@ package com.esgi.questions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("checkstyle:FinalClass")
 @SpringBootApplication
 public class QuestionApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(QuestionApplication.class, args);
     }
-
-    private QuestionApplication() { }
 }
--- questions/src/main/java/com/esgi/questions/domain/regleAttributionPoints/domainEvent/RegleSupprimee.java
+++ questions/src/main/java/com/esgi/questions/domain/regleAttributionPoints/domainEvent/RegleSupprimee.java
@@ -1,4 +1,9 @@
 package com.esgi.questions.domain.regleAttributionPoints.domainEvent;
 
 public class RegleSupprimee {
+    public Long regleAttributionPointsId;
+
+    public RegleSupprimee(Long regleAttributionPointsId) {
+        this.regleAttributionPointsId = regleAttributionPointsId;
+    }
 }
